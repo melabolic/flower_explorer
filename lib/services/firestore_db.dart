c0317abaf84ad9149ca9ff9c0f6af4394f452f47
melabolic <jiewen.lim@minerva.kgi.edu>
2020-04-23 16:47:24 +0800

    Added comments on how this code snippet simulates somewhat of a singleton effect for my app

--- lib/services/firestore_db.dart
+++ lib/services/firestore_db.dart
@@ -1,4 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+/* 
+  The 'final' keyword indicates that the variable will only be set once 
+  throughout this entire app, making this implementation very similar to 
+  the singleton design pattern - where the goal is to obtain a single instance
+  of the object.
+*/
 final _db = Firestore.instance;
 final collection = _db.collection("flowers");
